69831cbecf7a316b0dd6be77cccd456a50ae96d1
nandeeshpatelgp <nandeeshpatelgp@gmail.com>
2017-11-25 14:58:31 +0530

    Adding angular pipes demo.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po';
+import { PipesPage } from './app.po';
 
-describe('github App', () => {
-  let page: AppPage;
+describe('pipes App', () => {
+  let page: PipesPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new PipesPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ShortenPipe} from "./Shorten.pipe";
+import { FilterPipe } from './filter.pipe';
+import {FormsModule} from "@angular/forms";
+import { SortPipe } from './sort.pipe';
+import { ReversePipe } from './reverse.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortenPipe,
+    FilterPipe,
+    SortPipe,
+    ReversePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
